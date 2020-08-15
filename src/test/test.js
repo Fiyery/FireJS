e6a4f3b98d93a77dbe0d2ef42b090b3667395b17
Yoann Chaumin <yoann.chaumin@gmail.com>
2020-08-15 09:08:50 +0200

    0.4.6 - Function each() to iterate on Object

--- src/test/test.js
+++ src/test/test.js
@@ -9,7 +9,77 @@ fire.ready(function(){
 		fire.component.run();
 	});
 
+	let users = [
+		{
+		  "_id": "5f3786de502a56467286cbe9",
+		  "index": 0,
+		  "guid": "b57a2353-c222-4cc8-af28-1a576128fd39",
+		  "isActive": false,
+		  "age": 25,
+		  "name": {
+			"first": "Blankenship",
+			"last": "Hooper"
+		  },
+		  "company": "VURBO",
+		  "email": "blankenship.hooper@vurbo.org"
+		},
+		{
+		  "_id": "5f3786de30c6e903267c41fe",
+		  "index": 1,
+		  "guid": "5dedf6b2-700b-4963-a86f-6607457d25ec",
+		  "isActive": true,
+		  "age": 31,
+		  "name": {
+			"first": "Alana",
+			"last": "Hendricks"
+		  },
+		  "company": "RADIANTIX",
+		  "email": "alana.hendricks@radiantix.ca"
+		},
+		{
+		  "_id": "5f3786de9306ab6350b4afd4",
+		  "index": 2,
+		  "guid": "789ef49d-5ba2-4989-b0db-46c019e6941e",
+		  "isActive": true,
+		  "age": 20,
+		  "name": {
+			"first": "Lynnette",
+			"last": "Hobbs"
+		  },
+		  "company": "BALUBA",
+		  "email": "lynnette.hobbs@baluba.biz"
+		},
+		{
+		  "_id": "5f3786deebac91a18c93b7cd",
+		  "index": 3,
+		  "guid": "faa7e100-86b5-439b-8171-c446f5327d13",
+		  "isActive": true,
+		  "age": 24,
+		  "name": {
+			"first": "Sallie",
+			"last": "Terrell"
+		  },
+		  "company": "IMAGINART",
+		  "email": "sallie.terrell@imaginart.tv"
+		},
+		{
+		  "_id": "5f3786dece809aa0e727db80",
+		  "index": 4,
+		  "guid": "2217c3cc-49dc-49bd-a667-ed55dff5ab51",
+		  "isActive": true,
+		  "age": 25,
+		  "name": {
+			"first": "Lena",
+			"last": "Ramsey"
+		  },
+		  "company": "SUREMAX",
+		  "email": "lena.ramsey@suremax.info"
+		}
+	  ];
 
+	fire.each(users, (val, key) => {
+		console.log(key, "=> ", val);
+	});
 
 
 
